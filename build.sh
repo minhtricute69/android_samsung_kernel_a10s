f72651fe52d715e90977646537a21a24e4d850bd
minhtricute69 <minhtricute69@gmail.com>
2022-06-15 13:56:30 +0700

    Minor change

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo "Clean Build Environment" ;
-make clean && make mrproper ; 
 echo "Init ENV" ;
 export ANDROID_MAJOR_VERSION=q ;
 export PLATFORM_VERSION=10 ;
 export ARCH=arm64 ;
 export SUBARCH=arm64 ;
+export CC="clang" ;
+export CROSS_COMPILE="aarch64-linux-gnu-" ;
+echo "Clean Build ENV" ;
+make clean && make mrproper;
 echo "Building Kernel" ;
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physwizz_defconfig ;
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j2 ;
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CROSS_COMPILE="aarch64-linux-gnu-" CC="clang" physwizz_defconfig ;
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y CROSS_COMPILE="aarch64-linux-gnu-" CC="clang" -j2 ;
---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue May 24 13:22:09 +07 2022"
+#define LINUX_COMPILE_BY "tri"
+#define LINUX_COMPILE_HOST ""
+#define LINUX_COMPILER "SiLonT clang version 14.0.0 (https://github.com/llvm/llvm-project 79c2616d315f54d72bcdeebb6576c84be7b585d8)"
